7a2d81c9d411c799ba7a6f4e7746f0951a786c68
Gustavo Lima <gustavo.it@outlook.com>
2023-02-09 15:54:17 -0300

    Feat: rippleButton
    1. created a button with ripple effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import './App.scss';
 import { Button } from './components/Button';
+import { ButtonWithRipple } from './components/ButtonWithRipple'
 import { Slider } from './components/Slider';
 
 function App() {
 
 	return (
 		<div className='appContainer'>
-      <Slider />
-      <Button text='Click Me!'/>
-
+			<Slider />
+			<Button text='Click Me!' />
+			<ButtonWithRipple />
 		</div>
-	);
+	)
 }
 
 export default App;
---
+++ src/components/ButtonWithRipple/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from 'react'
+import './styles.scss'
+
+export function ButtonWithRipple() {
+	const buttonRef = useRef<HTMLButtonElement>(null)
+
+	function rippleEffect(event: any) {
+		if (!event?.target) return
+		if (!buttonRef?.current) return
+		if (!buttonRef.current.contains(event.target)) return
+		const button = buttonRef.current
+		const circle = document.createElement('span')
+		const diameter = Math.max(button.clientWidth, button.clientHeight)
+		const radius = diameter / 2
+
+		circle.style.width = circle.style.height = `${diameter}px`
+		circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`
+		circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`
+		circle.classList.add('ripple')
+
+		const ripple = button.getElementsByClassName('ripple')?.[0]
+
+		if (ripple) {
+			ripple.remove()
+		}
+
+		button.appendChild(circle)
+	}
+
+	useEffect(() => {
+		document.addEventListener('click', rippleEffect)
+
+		return () => {
+			document.removeEventListener('click', rippleEffect)
+		}
+	}, [])
+
+	return (
+		<button ref={buttonRef} className='buttonRipple'>
+			Ripple
+		</button>
+	)
+}
